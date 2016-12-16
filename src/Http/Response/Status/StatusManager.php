8258d79ef770c653625a41192b2beb9223903c60
scorsi <sylvain.corsini@gmail.com>
2016-12-16 15:34:36 +0100

    Adding Status.

---
+++ src/Http/Response/Status/StatusManager.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace Bootlace\Http\Response\Status;
+
+/**
+ * Class StatusManager
+ *
+ * @package Bootlace\Http
+ */
+class StatusManager implements StatusManagerInterface
+{
+    /* @var string $_statusText */
+    protected $statusText = "OK";
+
+    /* @var int $_statusCode */
+    protected $statusCode = 200;
+
+    /* @var string $protocolVersion */
+    protected $protocolVersion = '1.1';
+
+    /**
+     * StatusManager constructor.
+     *
+     * @param null|int $statusCode
+     * @param null|string $statusText
+     */
+    public function __construct(?int $statusCode = null, ?string $statusText = null)
+    {
+        if ($statusCode != null) {
+            $this->setStatus($statusCode, $statusText);
+        }
+    }
+
+    /**
+     * Sends Status.
+     */
+    public function send(): void
+    {
+        $this->httpStatusLine();
+    }
+
+    /**
+     * Set the StatusLine Header.
+     */
+    public function httpStatusLine(): void
+    {
+        header(sprintf('HTTP/%s %s %s',
+            trim($this->protocolVersion),
+            trim($this->statusCode),
+            trim($this->statusText)));
+    }
+
+    /**
+     * Set the protocol version (HTTP Version).
+     *
+     * @param string $protocolVersion
+     * @return StatusManagerInterface
+     */
+    public function setProtocolVersion(string $protocolVersion): StatusManagerInterface
+    {
+        $this->protocolVersion = $protocolVersion;
+        return $this;
+    }
+
+    /**
+     * Sets the HTTP status.
+     *
+     * @param int $statusCode
+     * @param null|string $statusText (optional)
+     * @return StatusManagerInterface
+     */
+    public function setStatus(int $statusCode, ?string $statusText = null): StatusManagerInterface
+    {
+        if ($statusText === null && array_key_exists($statusCode, self::STATUS_TEXTS)) {
+            $statusText = self::STATUS_TEXTS[$statusCode];
+        }
+        $this->statusCode = $statusCode;
+        $this->statusText = $statusText;
+        return $this;
+    }
+
+    /**
+     * Returns the HTTP status code.
+     *
+     * @return int
+     */
+    public function getStatusCode(): int
+    {
+        return $this->statusCode;
+    }
+
+    /**
+     * Returns the HTTP status text.
+     *
+     * @return string
+     */
+    public function getStatusText(): string
+    {
+        return $this->statusText;
+    }
+}
---
+++ src/Http/Response/Status/StatusManagerInterface.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Bootlace\Http\Response\Status;
+
+/**
+ * Interface StatusManagerInterface
+ *
+ * @package Bootlace\Http
+ */
+interface StatusManagerInterface
+{
+    /* @var array Status code with status text. */
+    public const STATUS_TEXTS = [
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+        208 => 'Already Reported',
+        226 => 'IM Used',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => 'Reserved',
+        307 => 'Temporary Redirect',
+        308 => 'Permanent Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Reserved for WebDAV advanced collections expired proposal',
+        426 => 'Upgrade Required',
+        428 => 'Precondition Required',
+        429 => 'Too Many Requests',
+        431 => 'Request Header Fields Too Large',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        508 => 'Loop Detected',
+        510 => 'Not Extended',
+        511 => 'Network Authentication Required',
+    ];
+
+    /**
+     * Sends the Status.
+     */
+    public function send(): void;
+
+    /**
+     * Sets the HTTP status.
+     *
+     * @param int $statusCode
+     * @param string $statusText (optional)
+     * @return StatusManagerInterface
+     */
+    public function setStatus(int $statusCode, ?string $statusText = null): StatusManagerInterface;
+
+    /**
+     * Returns the HTTP status code.
+     *
+     * @return int
+     */
+    public function getStatusCode(): int;
+
+    /**
+     * Returns the HTTP status text.
+     *
+     * @return string
+     */
+    public function getStatusText(): string;
+}
