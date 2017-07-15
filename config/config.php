e21a0d02789577c77c334f3fcea8ed7cd4791aaa
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-07-15 15:15:18 +0200

    [ADD] Adding QueryManager configuration."

--- config/config.php
+++ config/config.php
@@ -1,3 +1,10 @@
 <?php
 
 define('SECRET_KEY', 'c532c3f2b2574d1cf29ef3cdcd8912e7151b276cdd8d2752037302293321321f');
+
+define('DB_CONNECTOR', 'Mysql');
+define('DB_NAME', 'test');
+define('DB_HOST', 'localhost');
+define('DB_PORT', '3306');
+define('DB_USER', 'root');
+define('DB_PASS', '');
