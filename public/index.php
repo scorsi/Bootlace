942b3a3a43cf41684458793e1d72b162c95264ef
Sylvain Corsini <sylvain.corsini@gmail.com>
2017-07-15 15:42:26 +0200

    [REF] Better handling in index.php.

--- public/index.php
+++ public/index.php
@@ -2,19 +2,19 @@
 
 require_once '../vendor/autoload.php';
 require_once '../config/config.php';
+require_once '../library/autoload.php';
 
 error_reporting(E_ALL);
 ini_set('log_errors', 'On');
 ini_set('error_log', __DIR__ . '/../log/error.log');
 ini_set('display_errors', 'On');
 
-$whoops = new \Whoops\Run;
+$whoops = new \Whoops\Run();
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
 $whoops->register();
 
 \Kint_Renderer_Rich::$theme = 'solarized-dark.css';
 
-require_once '../library/autoload.php';
 
 $kernel = new \Bootlace\Kernel(
     __DIR__ . DIRECTORY_SEPARATOR . '..',
